dae4c8e841abe87428693671288f97ea833e12b9
Lucio Melis <lucio.melis@abinsula.com>
2026-05-27 15:38:00 +0200

    Created Staging branch

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -5,6 +5,7 @@ from apps.user.models import User
 
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
+
     list_display = DjangoUserAdmin.list_display + ("phone_number", "address", "city", "zip_code", "country", "deleted")
     fieldsets = DjangoUserAdmin.fieldsets + (
         ("Address",
