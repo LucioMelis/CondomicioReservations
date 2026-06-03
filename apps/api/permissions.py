a89119744b3ea94f9a056af12d1b68ae35ca01d3
Lucio Melis <lucio.melis@abinsula.com>
2026-06-03 11:17:27 +0200

    Added new folder api with new file permissions.py, into user/views.py added permissions

---
+++ apps/api/permissions.py
@@ -0,0 +1,21 @@
+from rest_framework.permissions import IsAuthenticated
+
+class ApiIsAuthenticatedPermission(IsAuthenticated):
+    pass
+
+class ApiIsAuthenticatedAsAdminPermission(ApiIsAuthenticatedPermission):
+    def has_permission(self, request, view):
+        permission = super(ApiIsAuthenticatedAsAdminPermission,self).has_permission(request, view)
+        if not permission:
+            return permission
+        user = request.user
+        return permission and user.groups.filter(name='Admin').exists()
+
+class ApiIsAuthenticatedAsCustomerPermission(ApiIsAuthenticatedPermission):
+    def has_permission(self, request, view):
+        permission = super(ApiIsAuthenticatedAsCustomerPermission,self).has_permission(request, view)
+        if not permission:
+            return permission
+        user = request.user
+        return permission and user.groups.filter(name='Customer').exists()
+
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,10 +1,10 @@
-from rest_framework.permissions import AllowAny
+from apps.api.permissions import ApiIsAuthenticatedAsAdminPermission
 from rest_framework.viewsets import ModelViewSet
 from django.http.response import HttpResponse
 
 
 class UserListView(ModelViewSet):
-    permission_classes = [AllowAny]
+    permission_classes = (ApiIsAuthenticatedAsAdminPermission,)
     def create(self,request,*args,**kwargs):
         response = HttpResponse("Hello, Henry you have a shit connection Man!")
         return response
