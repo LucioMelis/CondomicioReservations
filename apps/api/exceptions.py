82e353d338274a9250525c0dae8e7397816c3b4d
Lucio Melis <lucio.melis@abinsula.com>
2026-06-04 17:36:13 +0200

    Added two classes in exceptions.py file

--- apps/api/exceptions.py
+++ apps/api/exceptions.py
@@ -1,3 +1,6 @@
+from rest_framework import status
+
+
 class APIBaseException(Exception):
     data = None
     message = None
@@ -6,4 +9,17 @@ class APIBaseException(Exception):
         super(APIBaseException, self).__init__(message)
         self.message = message
         self.data = data
-        
+
+class APIException(APIBaseException):
+    api_error = None
+    http_status_code = status.HTTP_400_BAD_REQUEST
+
+    def __init__(self, message, api_error=400, data=None, http_status_code=status.HTTP_400_BAD_REQUEST, **kwargs):
+        super(APIException, self).__init__(message, data, **kwargs)
+        self.api_error = api_error
+        self.http_status_code = http_status_code
+
+
+class APIValidationException(APIException):
+    def __init__(self, message, api_error=400, data=None, **kwargs):
+        super(APIValidationException, self).__init__(message, api_error, data, status.HTTP_400_BAD_REQUEST, **kwargs)
